19bbd279bd0491717352452f020b51911efbdc71
Alena Kravtsova <a.kravtsova@datosydecisiones.es>
2026-03-09 12:15:40 +0100

    Modified main.tsx instantiating MSAL Public Client Application with our custom configurtion from msalConfig and wrapping our App with Msal Provider in order to use MSAL with React Hooks in entire App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { MsalProvider } from "@azure/msal-react";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from './config/msalConfig.ts';
+
+// Create a central instance of MSAL Browser with our specific configuration
+// It will allow us to carry out: login, token managing,  logout etc
+const msalInstance = new PublicClientApplication(msalConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* MsalProvider is React Context Provider that allows our SPA to use MSAL with React Hooks and it injects in entire React App */}
+    <MsalProvider instance={msalInstance}>
     <App />
+    </MsalProvider>
   </StrictMode>,
 )
