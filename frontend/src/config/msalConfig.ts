221af8689452b637dd75f4e72f05abb44a1d3f75
Alena Kravtsova <a.kravtsova@datosydecisiones.es>
2026-03-09 11:27:05 +0100

    Created MSAL Configuration for our Frontend

---
+++ frontend/src/config/msalConfig.ts
@@ -0,0 +1,18 @@
+// Defines main configuration of MSAL Browser for our React App
+import { type Configuration, type PopupRequest } from "@azure/msal-browser";
+
+export const msalConfig: Configuration = {
+    auth: {
+        clientId: import.meta.env.VITE_AZURE_CLIENT_ID!,
+        authority: import.meta.env.VITE_AZURE_AUTHORITY,
+        redirectUri: import.meta.env.VITE_AZURE_REDIRECT_URI
+    },
+    cache: {
+        cacheLocation: "localStorage",
+    }
+};
+
+// Define scopes that will be required
+export const loginRequest: PopupRequest = {
+    scopes: [import.meta.env.VITE_AZURE_SCOPE!]
+};
