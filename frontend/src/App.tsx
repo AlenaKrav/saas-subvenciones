5cfdae7d1c5dfaef982eb2fa48b151f9041968fc
Alena Kravtsova <a.kravtsova@datosydecisiones.es>
2026-03-10 11:42:45 +0100

    Modified App.tsx in order to implement Login, Logout, access to protected routes with MSAL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,166 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '../src/App.css';
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal, useIsAuthenticated } from '@azure/msal-react';
+import { loginRequest } from './config/msalConfig';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface UserInfo {
+    success: boolean;
+    data: {
+        userId: string;
+        email: string;
+        name?: string;
+    };
 }
 
-export default App
+
+function App() {
+    const { instance, accounts, inProgress } = useMsal();
+    const isAuthenticated = useIsAuthenticated();
+    const [loading, setLoading] = useState(false);
+    const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+    const [products, setProducts] = useState(null);
+
+
+
+    useEffect(() => {
+        const handleRedirect = async () => {
+            try {
+                const response = await instance.handleRedirectPromise();
+                if (response) {
+                    console.log("Successfull login redirect:", response.account);
+                }
+            } catch (error) {
+                console.error("Error ocurred during redirect:", error);
+            }
+        };
+
+        handleRedirect();
+    }, [instance]);
+
+
+
+    const handleLogin = async () => {
+        try {
+            await instance.loginRedirect(loginRequest);
+        } catch (error) {
+            console.error("Failed to log in:", error);
+        }
+    };
+
+    const handleLogout = async () => {
+        try {
+            await instance.logoutRedirect({
+                postLogoutRedirectUri: window.location.origin
+            });
+        } catch (error) {
+            console.error("Failed to log out:", error);
+        }
+    };
+
+    const fetchMe = async () => {
+        try {
+            setLoading(true);
+            const response = await instance.acquireTokenSilent({
+                ...loginRequest,
+                account: accounts[0]
+            });
+            //get protected route
+            const result = await axios.get(`${import.meta.env.VITE_API_URL}/auth/msal/me`, {
+                headers: {
+                    'Authorization': `Bearer ${response.accessToken}`
+                }
+            });
+            setUserInfo(result.data);
+            setLoading(false);
+        } catch (error) {
+            console.error('Error obtaining user info:', error);
+        }
+    };
+
+
+    const fetchProducts = async () => {
+        try {
+            setLoading(true);
+            const response = await instance.acquireTokenSilent({
+                ...loginRequest,
+                account: accounts[0]
+            });
+
+            const result = await axios.get(`${import.meta.env.VITE_API_URL}/products`, {
+                headers: {
+                    'Authorization': `Bearer ${response.accessToken}`
+                }
+            });
+            setProducts(result.data);
+            setLoading(false);
+        } catch (error) {
+            console.error('Error obtaining products:', error);
+        }
+    };
+
+    console.log('Authenticated: ', isAuthenticated);
+    console.log('Accounts: :', accounts);
+    console.log('In progress:', inProgress);
+
+    return (
+        <div className="app-container">
+            <h1> Authentication: React + MSAL + Fastify</h1>
+            <UnauthenticatedTemplate>
+                <div className="auth-box">
+                    <h2>You are not logged in</h2>
+                    <p>State: {inProgress}</p>
+                    
+                    <button
+                        className="btn btn-login"
+                        onClick={handleLogin}
+                        disabled={inProgress !== 'none'}
+                    >
+                        Log in
+                    </button>
+                </div>
+            </UnauthenticatedTemplate>
+
+            <AuthenticatedTemplate>
+                <div className="auth-box logged-in">
+                    <h2>Now you are logged in!</h2>
+                    {accounts.length > 0 && (
+                        <div className="info-box">
+                            <p><strong>User:</strong> {accounts[0].name}</p>
+                            <p><strong>Email:</strong> {accounts[0].username}</p>
+                        </div>
+                    )}
+
+                    <div className="flex-row">
+                        <button onClick={fetchMe} className="btn btn-check">Check protected user info</button>
+                    </div>
+
+                    {loading && <p>Loading user info...</p>}
+
+                    {!loading && userInfo && (
+                        <div className="info-box">
+                            <p><strong>Logged user id:</strong> {userInfo.data.userId}</p>
+                        </div>
+                    )}
+
+                    <div className="flex-row">
+                        <button onClick={fetchProducts} disabled={loading} className="btn btn-products">
+                            Get products
+                        </button>
+                    </div>
+
+                    {!loading && products && (
+                        <div>
+                            <h3>Products from Database:</h3>
+                            <pre className="info-box">{JSON.stringify(products, null, 2)}</pre>
+                        </div>
+                    )}
+
+                    <button onClick={handleLogout} className="btn btn-logout">Log out</button>
+                </div>
+            </AuthenticatedTemplate>
+        </div>
+    );
+}
+export default App;
