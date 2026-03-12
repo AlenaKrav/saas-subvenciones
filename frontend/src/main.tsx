796628e802a07b11480c208b709f04389d65546a
Alena Kravtsova <a.kravtsova@datosydecisiones.es>
2026-03-12 11:18:08 +0100

    Modified main.tsx adding interceptors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,15 @@ import App from './App.tsx'
 import { MsalProvider } from "@azure/msal-react";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { msalConfig } from './config/msalConfig.ts';
+import { setupInterceptors } from './services/apiClient.ts';
 
 // Create a central instance of MSAL Browser with our specific configuration
 // It will allow us to carry out: login, token managing, logout etc
 const msalInstance = new PublicClientApplication(msalConfig);
+
+//Configure interceptors
+setupInterceptors(msalInstance);
+
 // Use initilize with msalInstance to ensure that its ready and after we start rendering with React
 // It allows us to load cache, accounts, pending redirects and avoid some bugs
 msalInstance.initialize().then(() => {
